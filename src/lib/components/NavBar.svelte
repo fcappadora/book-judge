6944c35297630de61ab6d56601e6e8cc6d7fd5a5
franklintc <send2franklin@pm.me>
2025-07-01 20:22:27 -0700

    navigation and sub routes

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,13 +1,9 @@
-<div class="navbar bg-secondary rounded-4xl shadow-sm">
-	<div class="navbar-start">
-		<!-- <a class="btn btn-ghost text-xl">daisyUI</a> -->
-	</div>
+<div class="navbar bg-secondary flex justify-center rounded-4xl shadow-sm">
 	<div class="navbar-center flex">
 		<ul class="menu menu-horizontal px-1">
-			<li><a href="/">Judge a Book!</a></li>
-			<li><a href="/about-the-project">What is this?</a></li>
-			<li><a href="product-justiication">But why?</a></li>
+			<li><a class="text-lg" href="/">Judge a Book!</a></li>
+			<li><a class="text-lg" href="/about-the-project">What is this?</a></li>
+			<li><a class="text-lg" href="product-justiication">But why?</a></li>
 		</ul>
 	</div>
-	<div class="navbar-end"></div>
 </div>
