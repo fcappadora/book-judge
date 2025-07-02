98bf79cfa4e75ea12fa4ef29e02378e454819d1d
franklintc <send2franklin@pm.me>
2025-07-02 07:55:15 -0700

    some polish

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,7 +3,6 @@
 		<ul class="menu menu-horizontal px-1">
 			<li><a class="text-lg" href="/">Judge a Book!</a></li>
 			<li><a class="text-lg" href="/about-the-project">What is this?</a></li>
-			<li><a class="text-lg" href="product-justiication">But why?</a></li>
 		</ul>
 	</div>
 </div>
