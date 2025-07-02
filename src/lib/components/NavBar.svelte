b0108a49489f02a3ca2dac0f1fe58bc00fbc770c
franklintc <send2franklin@pm.me>
2025-07-01 19:22:17 -0700

    navigation and sub routes

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,13 @@
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="navbar-start">
+		<!-- <a class="btn btn-ghost text-xl">daisyUI</a> -->
+	</div>
+	<div class="navbar-center flex">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/">Judge a Book!</a></li>
+			<li><a href="/about-the-project">What is this?</a></li>
+			<li><a href="product-justiication">But why?</a></li>
+		</ul>
+	</div>
+	<div class="navbar-end"></div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import NavBar from '$lib/components/NavBar.svelte'
 	import '../app.css'
 
 	let { children } = $props()
 </script>
+
+<NavBar />
 
 {@render children()}
